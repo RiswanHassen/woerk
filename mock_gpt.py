325350de5b423121421c0a762bf946ca0516ff4d
RiswanHassen <riswanhassen@gmail.com>
2025-04-22 18:28:22 +0200

    sandbox mode
    adding sandbox mode for testing purposes

---
+++ mock_gpt.py
@@ -0,0 +1,6 @@
+
+def generate_intro(job_description, api_key=None):
+    return "[MOCK INTRO] " + job_description[::-1]
+
+def generate_cover_letter(job_description, cv_path, api_key=None, style_level=7):
+    return "[MOCK LETTER] " + job_description.upper()
