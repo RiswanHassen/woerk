9f12366ea5e7ae7d0e04c125fc30797b45590630
RiswanHassen <riswanhassen@gmail.com>
2025-04-22 19:56:48 +0200

    Update wizard_gui.py

--- wizard_gui.py
+++ wizard_gui.py
@@ -1,2 +1,22 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
 
-# Inhalt wurde im vorherigen Schritt definiert – wiederherstellen
+try:
+    from gpt_engine import generate_intro, generate_cover_letter
+except:
+    from mock_gpt import generate_intro, generate_cover_letter
+
+state = {
+    "level": "wenig",
+    "folder_path": "",
+    "resume_path": "",
+    "freitext": "",
+    "gehalt": "",
+    "waehrung": "EUR",
+    "api_sandbox": False,
+    "api_key": ""
+}
+
+def list_files_in_folder(path):
+    try:
